f3f151fab32f0b328204bf1e114f330d1754dee7
k0rsakov <korsakov.iyu@gmail.com>
2026-03-03 08:59:56 +0300

    Добавлен пример вставки данных в трино через пандас

---
+++ 06_integration_of_trino_into_de_python/simple_trino_sqlalchemy_dml_query.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from random import randint
+import uuid
+
+import pandas as pd
+
+# 1. Создаем тестовый датафрейм
+dict_ = {
+    'id': [randint(a=10, b=100) for _ in range(10)],
+    'value': [randint(a=100, b=1000) for _ in range(10)],
+    'str': [str(uuid.uuid4()) for _ in range(10)]
+}
+
+df = pd.DataFrame.from_dict(dict_)
+# 2. Создаём движок для работы с Trino
+engine = create_engine('trino://admin@localhost:8080/memory/default')
+
+# Вставка одним вызовом
+# if_exists='append' добавит данные, 'replace' пересоздаст таблицу
+df.to_sql(
+    name='foo',
+    con=engine,
+    if_exists='append',
+    index=False,
+    method='multi',
+)
