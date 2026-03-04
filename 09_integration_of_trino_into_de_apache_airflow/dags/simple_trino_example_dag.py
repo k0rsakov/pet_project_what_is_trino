c0a50c2e1e4395522a57147abde80b5f4027385b
k0rsakov <korsakov.iyu@gmail.com>
2026-03-04 09:26:20 +0300

    Оптимизирован код для даг

--- 09_integration_of_trino_into_de_apache_airflow/dags/simple_trino_example_dag.py
+++ 09_integration_of_trino_into_de_apache_airflow/dags/simple_trino_example_dag.py
@@ -10,17 +10,8 @@ from airflow.providers.trino.hooks.trino import TrinoHook
 
 # Конфигурация DAG
 OWNER = "i.korsakov"
-DAG_ID = "simple_dag"
+DAG_ID = "simple_trino_example_dag"
 
-LONG_DESCRIPTION = """
-# LONG DESCRIPTION
-
-"""
-
-SHORT_DESCRIPTION = "SHORT DESCRIPTION"
-
-# Описание возможных ключей для default_args
-# https://github.com/apache/airflow/blob/343d38af380afad2b202838317a47a7b1687f14f/airflow/example_dags/tutorial.py#L39
 args = {
     "owner": OWNER,
     "start_date": pendulum.datetime(year=2025, month=1, day=1, tz="UTC"),
@@ -68,12 +59,10 @@ with DAG(
     schedule_interval="0 10 * * *",
     default_args=args,
     tags=["context"],
-    description=SHORT_DESCRIPTION,
     concurrency=1,
     max_active_tasks=1,
     max_active_runs=1,
 ) as dag:
-    dag.doc_md = LONG_DESCRIPTION
 
     start = EmptyOperator(
         task_id="start",
